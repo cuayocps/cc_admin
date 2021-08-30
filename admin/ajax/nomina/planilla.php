32b494bd2eb941611514ce15981356e22ea4b941
Claudio Peralta <cuayocps@gmail.com>
2021-08-30 09:44:45 -0400

    descarga planilla báse para carga de nomina

---
+++ admin/ajax/nomina/planilla.php
@@ -0,0 +1,49 @@
+<?php
+require '../../modelos/Departamento.php';
+require '../../modelos/Tipousuario.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$cols = 'abcdefghijklmnopqrstuvwxyz';
+$columns = [
+    'Tipo usuario',
+    'Departamento',
+    'Nombre',
+    'Apellidos',
+    'Email',
+    'Login',
+    'Clave de ingreso',
+    'Clave de asistencia'
+];
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setTitle('Nómina');
+foreach($columns as $col => $title) {
+    $sheet->setCellValue("{$cols[$col]}1", $title);
+}
+
+$tipousuario = new Tipousuario();
+$tipos = $tipousuario->select()->fetch_all(MYSQLI_ASSOC);
+$sheet_2 = $spreadsheet->createSheet();
+$sheet_2->setTitle('Tipos de Usuario');
+foreach($tipos as $i => $tipo) {
+    $col = $i + 1;
+    $sheet_2->setCellValue("a{$col}", "{$tipo['idtipousuario']}:{$tipo['nombre']}");
+}
+
+$departamento = new Departamento();
+$departamentos = $departamento->select()->fetch_all(MYSQLI_ASSOC);
+$sheet_3 = $spreadsheet->createSheet();
+$sheet_3->setTitle('Departamentos');
+foreach($departamentos as $i => $dpto) {
+    $col = $i + 1;
+    $sheet_3->setCellValue("a{$col}", "{$dpto['iddepartamento']}:{$dpto['nombre']}");
+}
+
+$writer = new Xlsx($spreadsheet);
+
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="nomina_usuarios.xlsx"');
+header('Cache-Control: max-age=0');
+$writer->save('php://output');
--- admin/config/global.php
+++ admin/config/global.php
@@ -1,9 +1,14 @@
 <?php
+date_default_timezone_set('America/Santiago');
+
+require_once '../../../vendor/autoload.php';
+
+// error_reporting(E_ALL);
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "host.docker.internal");
 
 // nombre de la base de datos
-define("DB_NAME", "control_asistencia");
+define("DB_NAME", "ccadmin");
 
 
 //nombre de usuario de base de datos
@@ -11,11 +16,11 @@ define("DB_USERNAME", "root");
 //define("DB_USERNAME", "u222417_admin");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", ".sweetpwd.");
 //define("DB_PASSWORD", "Enero2020Admin");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
 
 //nombre del proyecto
-define("PRO_NOMBRE", "CompartiendoCodigos");
+define("PRO_NOMBRE", "_");
